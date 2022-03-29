5261cd4006ccc7080809962f485d8c23f524cecc
ALEKSEY KIRILOV <62812216+AlexeyTeacher@users.noreply.github.com>
2022-03-29 14:32:20 +0300

    Update settings.py

--- lucky_number/settings.py
+++ lucky_number/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yxue28n0)5*-i%ofnyqvlf6f*b1*w7$q!3@a$dy-w_=uh26r72'
+SECRET_KEY = 'input your secret key django auth'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
